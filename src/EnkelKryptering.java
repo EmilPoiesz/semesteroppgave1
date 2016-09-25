e08f42c98eae069b53caa258835113c45151204d
EmilPoiesz <EmilPoiesz@hotmail.com>
2016-09-26 00:35:58 +0200

    Enkel Kryptering almost done, some few bugs in encrypting when below 'a'

--- src/EnkelKryptering.java
+++ src/EnkelKryptering.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Emil on 21.09.2016.
  */
@@ -5,5 +7,36 @@ public class EnkelKryptering {
 
     public static void main (String args[]){
 
+        Scanner sc = new Scanner(System.in);
+        String innTekst, utTekst;
+        int noekkel;
+
+        System.out.println("Inn-tekst:");
+        innTekst = sc.nextLine();
+        System.out.println("Noekkel-verdi:");
+        noekkel = sc.nextInt();
+        utTekst = krypteer(innTekst, noekkel);
+        System.out.println("Ut-tekst:  " + utTekst);
+
+    }
+
+    private static String krypteer(String text, int key){
+        String returnString = "";
+        for(int i = 0; i < text.length(); i++){
+            if(text.charAt(i) >= 'a' && text.charAt(i) <= 'z'){
+                char currentChar = (char) (text.charAt(i)+(key%('z'-'a')));
+                if(currentChar > 'z'){
+                    returnString += (char)((currentChar % 'z')+ 'a' - 1);
+                }else if(currentChar < 'a'){ //less then a doesnt work
+                    returnString += (char)('z' - ('a' - currentChar - 1));
+                }else{
+                    returnString += currentChar;
+                }
+            }else{
+                returnString += (char)(text.charAt(i));
+            }
+        }
+
+        return returnString;
     }
 }
